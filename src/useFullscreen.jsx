bfe9d5097cf8a3ffaccd46efb57cfb99558bcfa5
minnnnnk0 <kjjpyckjs@naver.com>
2024-07-20 17:14:24 +0900

    fix: element function 조건 추가

--- src/useFullscreen.jsx
+++ src/useFullscreen.jsx
@@ -2,19 +2,38 @@ import { useRef } from 'react';
 
 const useFullscreen = (callback) => {
   const element = useRef();
+
+  // 중복으로 검사하는 부분을 하나로 통합
+  const runCb = (isfull) => {
+    if (callback && typeof callback === 'function') {
+      callback(isfull);
+    }
+  };
   const triggerFull = () => {
     if (element.current) {
       element.current.requestFullscreen();
-      if (callback && typeof callback === 'function') {
-        callback(true);
-      }
+    } else if (element.current.mozRequestFullScreen) {
+      element.current.mozRequestFullScreen();
+    } else if (element.current.webkitRequestFullscreen) {
+      element.current.webkitRequestFullscreen();
+    } else if (element.current.msRequestFullscreen) {
+      element.current.msRequestFullscreen();
     }
+    runCb(true);
   };
+
   const exitFull = () => {
     document.exitFullscreen();
-    if (callback && typeof callback === 'function') {
-      callback(false);
+    if (element.current) {
+      element.current.requestFullscreen();
+    } else if (element.current.mozRequestFullScreen) {
+      element.current.mozRequestFullScreen();
+    } else if (element.current.webkitRequestFullscreen) {
+      element.current.webkitRequestFullscreen();
+    } else if (element.current.msRequestFullscreen) {
+      element.current.msRequestFullscreen();
     }
+    runCb(false);
   };
   return { element, triggerFull, exitFull };
 };
